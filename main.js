fd496a4466384863bbc7130d51055e872f07c2bf
Brainfeed-1996 <121355278+Brainfeed-1996@users.noreply.github.com>
2024-02-06 17:40:36 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector("button");
+
+const alerte = () => {
+  alert("coucou");
+};
+// function alerte (){
+//   alert("coucou")
+// }
+
+btn.addEventListener("click", alerte);
